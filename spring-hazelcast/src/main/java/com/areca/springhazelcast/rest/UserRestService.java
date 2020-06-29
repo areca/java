a164775fb831dd12bb037194a5bbce2d9e491746
areca <kserin@gmail.com>
2020-06-29 13:25:18 +0300

    hazelcast mapstore added

--- spring-hazelcast/src/main/java/com/areca/springhazelcast/rest/UserRestService.java
+++ spring-hazelcast/src/main/java/com/areca/springhazelcast/rest/UserRestService.java
@@ -1,7 +1,8 @@
 package com.areca.springhazelcast.rest;
 
-import com.areca.springhazelcast.dao.UserRepository;
 import com.areca.springhazelcast.domain.User;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,23 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserRestService {
 
-    private UserRepository userRepository;
+    private IMap<String, User> userMap;
 
     @Autowired
-    public UserRestService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserRestService(HazelcastInstance hazelcastInstance){
+        this.userMap = hazelcastInstance.getMap("userMap");
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/user/{username}")
     public User getUserWithUsername(@PathVariable(value = "username") final String username) {
-        return userRepository.findByUsername(username);
+        return userMap.get(username);
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/user/{username}")
-    public void updateUserWithUsername(@PathVariable(value = "username") final String username,
-                                       @RequestBody final User user) {
+    public void updateUserWithUsername(@PathVariable(value = "username") final String username, final User user) {
         assert username.equals(user.getUsername());
-        userRepository.save(user);
+        userMap.put(username, user);
     }
 
 }
