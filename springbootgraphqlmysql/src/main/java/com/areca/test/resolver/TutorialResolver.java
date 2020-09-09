a7fb32bae4042add1105ad4a33404f9671377aa3
areca <kserin@gmail.com>
2020-09-09 11:01:17 +0300

    add project

--- springbootgraphqlmysql/src/main/java/com/areca/test/resolver/TutorialResolver.java
+++ springbootgraphqlmysql/src/main/java/com/areca/test/resolver/TutorialResolver.java
@@ -13,10 +13,6 @@ public class TutorialResolver implements GraphQLResolver<Tutorial> {
     @Autowired
     private AuthorRepository authorRepository;
 
-   /* public TutorialResolver(AuthorRepository authorRepository) {
-        this.authorRepository = authorRepository;
-    }*/
-
     public Author getAuthor(Tutorial tutorial) {
         return authorRepository.findById(tutorial.getAuthor().getId()).orElseThrow(null);
     }
