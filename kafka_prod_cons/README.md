10c7e46b9a73b157d86d4370306148306b932336
areca <kserin@gmail.com>
2020-09-15 09:46:56 +0300

    add kafka example

---
+++ kafka_prod_cons/README.md
@@ -0,0 +1,2 @@
+Basic Kafka Producer Consumer example
+tut from https://www.udemy.com/course/apache-kafka-and-spring-boot-consumer-producer/learn/lecture/14836700#overview
---
+++ kafka_prod_cons/src/test/java/com/areca/test/kafka/KafkaProdConsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.areca.test.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProdConsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
